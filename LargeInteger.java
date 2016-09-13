eb8548603e85d222cbcfebaa50e09baccd69f817
Jaceeee <jtroldan@up.edu.ph>
2016-09-13 13:05:08 +0800

    Add files via upload

---
+++ LargeInteger.java
@@ -0,0 +1,102 @@
+import java.util.*;
+
+public class LargeInteger{
+	private int size;
+	private String number;
+
+	LargeInteger() {
+
+	}
+
+	LargeInteger(String numString) {
+		number = numstring;
+		size = numString.length();
+	}
+
+	LargeInteger(LargeInteger numString) {
+		number = numString.getNumber
+
+	}
+
+	LargeInteger(int number) {
+		number = Integer.toString(number);
+		size = number.length();
+	}
+
+	public LargeInteger add(LargeInteger a){
+		LargeInteger ans;
+		return ans;
+	}
+
+	public LargeInteger subtract(LargeInteger a){
+		LargeInteger ans;
+		return ans;
+	}
+
+	public LargeInteger multiply(LargeInteger a){
+		LargeInteger ans;
+		return ans;
+	}
+
+	public LargeInteger divide(LargeInteger a){
+		LargeInteger ans;
+		return ans;
+	}
+
+	public LargeInteger add(String a){
+		LargeInteger ans;
+		return ans;
+	}
+
+	public LargeInteger subtract(String a){
+		LargeInteger ans;
+		return ans;
+	}
+
+	public LargeInteger multiply(String a){
+		LargeInteger ans;
+		return ans;
+	}
+
+	public LargeInteger divide(String a){
+		LargeInteger ans;
+		return ans;
+	}
+
+	public LargeInteger add(int a){
+		LargeInteger ans;
+		return ans;
+	}
+
+	public LargeInteger subtract(int a){
+		LargeInteger ans;
+		return ans;
+	}
+
+	public LargeInteger multiply(int a){
+		LargeInteger ans;
+		return ans;
+	}
+
+	public LargeInteger divide(int a){
+		LargeInteger ans;
+		return ans;
+	}
+
+	LargeInteger toString(){
+
+	}
+
+	public void display(){
+		System.out.println(getNumber());
+	}
+
+	boolean equals(LargeInteger){
+
+	}
+	
+	public getNumber(){ return this.number; }
+	
+	public getSize(){ return this.size; }
+}
+
